26ee1acae365fba4c8d1aab6c3b431defa8b7940
OsosKP <ososkp@gmail.com>
2019-09-12 09:50:40 +0100

    Split app into several modules for features, shared components and core services

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AuthComponent } from './auth.component';
+import { SharedModule } from '../shared/shared.module';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild([
+      { path: 'auth', component: AuthComponent }
+    ])
+  ]
+})
+export class AuthModule {}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlertComponent } from './alert/alert.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
+import { DropdownDirective } from './dropdown/dropdown.directive';
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective
+  ],
+  imports: [
+    CommonModule // Use to get ngIf and ngFor outside of app.module, which has those through BrowserModule
+  ],
+  // Export to use these components and directives in other modules
+  exports: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropdownDirective,
+    CommonModule
+  ]
+})
+export class SharedModule {}
