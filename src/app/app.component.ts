b7b354bce44c6a217ed5de024dc356358a977f99
OsosKP <ososkp@gmail.com>
2019-10-16 08:26:36 +0100

    Add Nest.js changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID, InjectionToken } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
@@ -12,10 +13,14 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
   constructor(private store: Store<fromApp.AppState>,
-              private loggingService: LoggingService) {}
+              private loggingService: LoggingService,
+              @Inject(PLATFORM_ID) private platformId: InjectionToken<Object>
+  ) {}
 
   ngOnInit () {
-    this.store.dispatch(new AuthActions.AutoLogin);
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin);
+    }
     this.loggingService.printLog('Hello from AppComponent ngOnInit.')
   }
 }
